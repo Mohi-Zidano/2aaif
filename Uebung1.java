f81c77394cdfff3fbb9c33ad39accc65346bc469
Ich <123affe@hotmail.com>
2017-05-11 20:15:11 +0200

    erste Version

---
+++ Uebung1.java
@@ -0,0 +1,21 @@
+package GitSource
+
+public class Uebung1
+{
+	private String name;
+	
+	public Uebung1(String name)
+	{
+		setName(name);
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+}
